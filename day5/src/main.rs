43e03470a0754be2b37bea4d8e1cf5d07eb8cdc1
Dane Larsen <dane.t.larsen@gmail.com>
2024-12-04 23:58:12 -0700

    sorting aaaaaah

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use aoclib::AocData;
 
 fn test_rule(print_set: &[usize], rule: &(usize, usize)) -> bool {
@@ -39,13 +41,20 @@ fn reorder(print_set: &[usize], broken_rule: &(usize, usize)) -> Vec<usize> {
 }
 
 fn reorder_all(print_set: &[usize], rules: &[(usize, usize)]) -> Vec<usize> {
-    let mut print_set = Vec::from(print_set);
-    while !set_ordered(&print_set, rules) {
+    let sort_fn = |a: &usize, b: &usize| -> Ordering {
         for rule in rules {
-            print_set = reorder(&print_set, rule);
+            if rule.0 == *a && rule.1 == *b {
+                return Ordering::Less;
+            } else if rule.0 == *b && rule.1 == *a {
+                return Ordering::Greater;
+            }
         }
-    }
 
+        Ordering::Equal
+    };
+
+    let mut print_set = Vec::from(print_set);
+    print_set.sort_by(sort_fn);
     print_set
 }
 
